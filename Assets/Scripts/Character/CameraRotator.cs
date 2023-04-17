ec8c0ab916d7f453b95b37049839e58b799a54e1
Yaroslav <yaroslavkyzyk@gmail.com>
2023-04-17 20:37:22 +0300

    Added Maps for Textures + Character Rotation

---
+++ Assets/Scripts/Character/CameraRotator.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+
+namespace Forest.Character
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraRotator : MonoBehaviour
+    {
+        private const float MaxRotationAngle = 88f;
+        private const float MinRotationAngle = -88f;
+
+        private Camera _camera;
+        private Quaternion _startRotation;
+
+        private float _xRotation = 0f;
+
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+            _startRotation = _camera.transform.rotation;
+        }
+
+        public void RotateCamera(float angle)
+        {
+            _xRotation += angle;
+            _xRotation = Mathf.Clamp(_xRotation, MinRotationAngle, MaxRotationAngle);
+
+            var rawRotationValue = Quaternion.Euler(_xRotation, 0f, 0f);
+
+            _camera.transform.localRotation = Quaternion.Inverse(rawRotationValue);
+        }
+
+        public void ResetRotation()
+        {
+            _camera.transform.rotation = _startRotation;
+        }
+    }
+}
--- Assets/Scripts/Character/CharacterControllerCustom.cs
+++ Assets/Scripts/Character/CharacterControllerCustom.cs
@@ -6,21 +6,52 @@ namespace Forest.Character
     [RequireComponent(typeof(CharacterController))]
     public class CharacterControllerCustom : MonoBehaviour
     {
-        [SerializeField] private float _speed;
+        [SerializeField] private CameraRotator _cameraRotator;
+
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _mouseSensetivity = 100f;
 
         private CharacterController _characterController;
         
-        private Vector3 MoveDireaction => new Vector3(Input.GetAxis("Vertical"), 0f , -Input.GetAxis("Horizontal")).normalized;
+        private Vector3 MoveDirection => new Vector3(Input.GetAxis("Vertical"), 0f , -Input.GetAxis("Horizontal")).normalized;
 
-
-        private void Update()
-        {
-            _characterController.SimpleMove(MoveDireaction * Time.deltaTime * _speed);
-        }
 
         private void Awake()
         {
             _characterController = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            var mouseX = Input.GetAxis("Mouse X") * _mouseSensetivity * Time.deltaTime;
+            var mouseY = Input.GetAxis("Mouse Y") * _mouseSensetivity * Time.deltaTime;
+
+            RotateCharacterByY(mouseX);
+            RotateCamera(mouseY);
+            MoveCharacter();
+
+            CheckForCameraRotationReset();
+        }
+
+        private void RotateCharacterByY(float angle)
+        {
+            transform.Rotate(Vector3.up * angle);
+        }
+
+        private void RotateCamera(float angle)
+        {
+            _cameraRotator.RotateCamera(angle);
+        }
+
+        private void CheckForCameraRotationReset()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                _cameraRotator.ResetRotation();
+        }
+
+        private void MoveCharacter()
+        {
+            _characterController.SimpleMove(MoveDirection * Time.deltaTime * _moveSpeed);
         }
     } 
 }
